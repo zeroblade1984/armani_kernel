573f6b405ffe0770d1bbc0ccb8327122013e8147
zeroblade1984 <zeroblade1984@gmail.com>
2015-01-12 09:02:50 +0800

    Compilation Fixes
    Signed-off-by: zeroblade1984 <zeroblade1984@gmail.com>

--- drivers/misc/tspdrv/tspdrv.c
+++ drivers/misc/tspdrv/tspdrv.c
@@ -31,7 +31,7 @@
 #include <linux/platform_device.h>
 #include <asm/uaccess.h>
 #include <asm/atomic.h>
-#include <tspdrv.h>
+#include "tspdrv.h"
 
 static int g_nTimerPeriodMs = 5; /* 5ms timer by default. This variable could be used by the SPI.*/
 
@@ -40,7 +40,7 @@ static int g_nTimerPeriodMs = 5; /* 5ms timer by default. This variable could be
 static atomic_t g_bRuntimeRecord;
 #endif
 
-#include <ImmVibeSPI.c>
+#include "ImmVibeSPI.c"
 #if (defined(VIBE_DEBUG) && defined(VIBE_RECORD)) || defined(VIBE_RUNTIME_RECORD)
 #include <tspdrvRecorder.c>
 #endif
@@ -81,9 +81,9 @@ static int g_nMajor = 0;
 
 
 /* Needs to be included after the global variables because they use them */
-#include <tspdrvOutputDataHandler.c>
+#include "tspdrvOutputDataHandler.c"
 #ifdef CONFIG_HIGH_RES_TIMERS
-    #include <VibeOSKernelLinuxHRTime.c>
+    #include "VibeOSKernelLinuxHRTime.c"
 #else
     #include <VibeOSKernelLinuxTime.c>
 #endif
