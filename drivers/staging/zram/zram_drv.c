1b16d6eb2923d2e1ffbfc26a967199404e964d32
zeroblade1984 <zeroblade1984@gmail.com>
2015-04-01 20:03:29 +0800

    My personal tweaks to ZRAM

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -47,7 +47,7 @@ static struct zram *zram_devices;
 #define ALLOC_ERROR_LOG_RATE_MS 1000
 
 /* Module params (documentation at end) */
-static unsigned int num_devices = 1;
+static unsigned int num_devices = 4;
 
 static inline struct zram *dev_to_zram(struct device *dev)
 {
